9da4bfdd93287fa8b146daaf92034fa38af52668
Eiekama <42797046+Eiekama@users.noreply.github.com>
2026-02-18 15:32:55 -0500

    environment wrapper
    writing an environment class to unify arc agi and atari environments. have the skeleton done but haven't written the main code for transforming the state and actions yet. was gonna start on state but not sure how to detect when we reset the game for arc agi (i need that information to know when to regenerate the color map). am going to write a play script where a human can play a game and have state information print to the terminal to help with understanding the environment.

--- datasets.py
+++ datasets.py
@@ -1,33 +1,92 @@
 import arc_agi
-from arcengine import GameAction
-import numpy
+from arcengine import GameAction, FrameDataRaw, GameState, SimpleAction, ComplexAction
+import gymnasium as gym
+import numpy as np
+from enum import Enum
+from typing import Literal
 
 class Environment:
-    '''
-    An environment wrapper to standardize the interface for different datasets e.g. gymnasium, arc-agi
-    '''
-    def __init__(self, env: arc_agi.EnvironmentWrapper):
-        self.env = env
+    class Benchmark(Enum):
+        ARC = "arc"
+        ATARI = "atari"
+
+    def __init__(
+        self,
+        benchmark: str,
+        env: arc_agi.EnvironmentWrapper | gym.Env
+    ):
+        if (benchmark not in self.Benchmark):
+            raise ValueError(f"[Environment] Unsupported benchmark: {benchmark}")
+        
+        self._env = env
+        self.benchmark = self.Benchmark(benchmark)
+
+        # Indices 0-7 will be 0-1 inputs for the respective arc-agi actions and 8-9 will contain (x, y) for ACTION6
+        self.action_dim = (10,)
+        # The raw game state will be represented as a 64x64 RGB image
+        self.observation_dim = (3, 64, 64)
 
     @property
-    def action_dim(self):
-        return len(self.env.action_space)
-
-    @property
-    def observation_dim(self):
-        return len(self.env.observation_space.frame), *self.env.observation_space.frame[0].shape
-
-    def reset(self):
-        obs = self.env.reset()
-        return obs # probably need to process to be consistent with gymnasium
-
-    def step(self, action):
-        pass
-
-class ARCAGIEnv(Environment):
-    def __init__(self, env):
-        super().__init__(env)
+    def as_arc(self) -> arc_agi.EnvironmentWrapper:
+        assert isinstance(self._env, arc_agi.EnvironmentWrapper)
+        return self._env
     
+    @property
+    def as_gym(self) -> gym.Env:
+        assert isinstance(self._env, gym.Env)
+        return self._env
+
+    def unify_obs(
+        self,
+        obs: FrameDataRaw | np.ndarray[tuple[Literal[210], Literal[160], Literal[3]]]
+    ) -> np.ndarray[tuple[Literal[3], Literal[64], Literal[64]]]:
+        '''
+        Maps the different state spaces from various benchmarks to a common representation
+
+        :param obs: observation returned by env.step() or env.reset()
+        :return observation (ndarray): rgb image of shape (3, 64, 64)
+        '''
+        if self.benchmark == self.Benchmark.ARC:
+            #TODO: colormap indices
+            # the map is constant for each game and [re]randomized each time a new game (not level) is started
+            pass
+        elif self.benchmark == self.Benchmark.ATARI:
+            #TODO: downsize to 64x64
+            # do we preserve aspect and randomize padding? or stretch to fit?
+            pass
+
+        raise NotImplementedError
+
+    def reset(self) -> np.ndarray[tuple[Literal[3], Literal[64], Literal[64]]]:
+        if self.benchmark == self.Benchmark.ARC:
+            obs = self.as_arc.reset()
+        elif self.benchmark == self.Benchmark.ATARI:
+            obs, _ = self.as_gym.reset()
+        return self.unify_obs(obs) # can consider adding an info dict if it's helpful later
+
+    def step(self, action, xy=None) -> tuple[np.ndarray[tuple[Literal[3], Literal[64], Literal[64]]], bool, bool]:
+        '''
+        :param uint8 action: An index in the range [0, 7] that maps to the respective arc-agi action
+        :param (uint16, uint16) xy: If action is ACTION6, xy should contain coordinates for the action
+        :return observation (ndarray): RGB image of shape (3, 64, 64)
+        :return done (bool): Whether episode has ended. If true, user needs to call reset()
+        :return won (bool): Whether the episode was won (only true if done is true)
+        '''
+        if self.benchmark == self.Benchmark.ARC:
+            if action == 6:
+                assert xy is not None, "[Environment] xy coordinates must be provided for ACTION6"
+                obs = self.as_arc.step(GameAction(action, ComplexAction), {"x": xy[0], "y": xy[1]})
+            else:
+                obs = self.as_arc.step(GameAction(action, SimpleAction))
+            done = obs.state == GameState.WIN or obs.state == GameState.GAME_OVER
+            won = obs.state == GameState.WIN
+        elif self.benchmark == self.Benchmark.ATARI:
+            obs, reward, terminated, truncated, _ = self.as_gym.step(action)
+            done = terminated or truncated
+            won = reward > 0 and terminated #TODO: might need to verify this assumption
+
+        return self.unify_obs(obs), done, won # can consider adding an info dict if it's helpful later
+
 
 if __name__ == "__main__":
     import sys
@@ -39,14 +98,8 @@ if __name__ == "__main__":
 
     # games = arc.get_environments()
     # for game in games:
+    #     print(game)
     #     print(f"{game.game_id}: {game.title}")
 
-    env = arc.make("ls20", render_mode="terminal")
+    env = arc.make("ls20")
     frame = env.observation_space.frame[0]
-    norm_arr = (frame - frame.min()) / (frame.max() - frame.min())
-
-    plt.figure(figsize=(8, 8))
-    plt.imshow(norm_arr, cmap='tab20', interpolation='nearest')  # 'tab20' gives 20 distinct colors
-    plt.colorbar()  # Shows value-to-color mapping
-    plt.axis('off')
-    plt.show()
